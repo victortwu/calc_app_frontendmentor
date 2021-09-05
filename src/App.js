3109181f4adeb72a2e0b53bc0a7fd2a2eba96b25
victortwu <victor607@gmail.com>
2021-09-04 19:46:05 -0700

    added calculator functionality

--- src/App.js
+++ src/App.js
@@ -3,35 +3,126 @@ import './App.css';
 
 function App() {
 
+const dotRef = React.useRef()
+const displayRef = React.useRef()
+
 const [numberButtons, setNumberButtons] = useState([])
-// const [previousNumber, setPreviousNumber] = useState('')
+const [previousNumber, setPreviousNumber] = useState('')
 const [currentNumber, setCurrentNumber] = useState('')
-// const [operation, setOperation] = useState('')
+const [operation, setOperation] = useState('')
+const [dotOK, setDotOK] = useState(true)
 
 let tempNumBtnArr = []
 
-const addNumberButtons = () => {
-  let btnNode
-  for (let i = 0; i < 10; i++) {
-    if(i === 9) {
-      btnNode = {id: `btn0`, value: `0`}
-    } else {
-      btnNode = {id: `btn${i + 1}`, value: `${i + 1}`}
-    }
-    tempNumBtnArr.push(btnNode)
-  }
-}
 
-const displayChar =(char)=> {
-  setCurrentNumber(char)
-}
+      const addNumberButtons = () => {
+        let btnNode
+        for (let i = 0; i < 10; i++) {
+          if(i === 9) {
+            btnNode = {id: `btn0`, value: `0`}
+          } else {
+            btnNode = {id: `btn${i + 1}`, value: `${i + 1}`}
+          }
+          tempNumBtnArr.push(btnNode)
+        }
+      }
+
+
+      const calculate = (nextOperator) => {
+        let calculation
+
+        const numOne = parseFloat(previousNumber)
+        const numTwo = parseFloat(currentNumber)
+
+        if (isNaN(numOne) || isNaN(numTwo)) return
+
+        switch(operation) {
+
+          case '+':
+            calculation = numOne + numTwo
+          break
+
+          case '-':
+            calculation = numOne - numTwo
+          break
+
+          case 'x':
+            calculation = numOne * numTwo
+          break
+
+          case '/':
+            calculation = numOne / numTwo
+          break
+
+          default:
+            return
+        }
+        setCurrentNumber('')
+        setPreviousNumber(calculation.toString() + nextOperator)
+        setOperation(nextOperator)
+        setDotOK(true)
+        if(nextOperator === '') {
+          setCurrentNumber(calculation.toString())
+          setPreviousNumber('')
+        }
+      }
+
+
+      const displayChar =(char)=> {
+        if(char === ' . ') return
+        setCurrentNumber(currentNumber + char)
+      }
+
+
+      const selectOperation = (operator) => {
+        setOperation(operator)
+        if (currentNumber === '') return
+        if (previousNumber !== '') {
+          calculate(operator)
+        }
+        setPreviousNumber(currentNumber + operator)
+        setDotOK(true)
+        setCurrentNumber('')
+        if(currentNumber && previousNumber) {
+          calculate(operator)
+        }
+      }
+
+      const equals = () => {
+        calculate('')
+        
+      }
+
+
+      const backSpace = () => {
+        const content = displayRef.current.innerHTML
+
+        setCurrentNumber(currentNumber.slice(0, -1))
+        if(!currentNumber) {
+          const deletingPrevNum = previousNumber.slice(0, -1)
+          setCurrentNumber(deletingPrevNum)
+          setPreviousNumber('')
+        }
+        if (!content.includes('.')) {
+          setDotOK(true)
+        }
+      }
+
+
+      const reset = () => {
+        setPreviousNumber('')
+        setCurrentNumber('')
+        setOperation('')
+        setDotOK(true)
+      }
+
 
 useEffect(() => {
   addNumberButtons()
   setNumberButtons(tempNumBtnArr)
 }, [])
 
-console.log(numberButtons)
+
   return (
     <main>
         <div className='calculatorContainer'>
@@ -40,12 +131,12 @@ console.log(numberButtons)
           <div className='themeSwitch'>theme switch</div>
           </header>
 
-          <div className='display'>
-            {currentNumber}
+          <div ref={displayRef} className='display'>
+            {previousNumber}{currentNumber}
           </div>
 
           <div className='keyPad'>
-            <div id='del'>DEL</div>
+            <div id='del' onClick={()=> backSpace()}>DEL</div>
             {
               numberButtons.map(btn=> {
                 return <button
@@ -57,13 +148,24 @@ console.log(numberButtons)
                         </button>
               })
             }
-            <button id='dot'>.</button>
-            <button id='plus'>+</button>
-            <button id='minus'>-</button>
-            <button id='times'>x</button>
-            <button id='divide'>/</button>
-            <div id='reset'>RESET</div>
-            <div id='equals'>=</div>
+            <button
+              id='dot'
+              ref={dotRef}
+              onClick={() => {
+                displayChar(dotRef.current.innerHTML)
+                setDotOK(false)
+              }}
+              >
+            {
+              dotOK ? '.' : ' . '
+            }
+            </button>
+            <button id='plus' onClick={()=> selectOperation('+')}>+</button>
+            <button id='minus' onClick={()=> selectOperation('-')}>-</button>
+            <button id='times' onClick={()=> selectOperation('x')}>x</button>
+            <button id='divide' onClick={()=> selectOperation('/')}>/</button>
+            <div id='reset' onClick={()=> reset()}>RESET</div>
+            <div id='equals' onClick={()=> equals()}>=</div>
           </div>
 
 
